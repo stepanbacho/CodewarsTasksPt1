91456d95b40227dff2a4d0f952f59820acd6c4da
stepanbacho <bachostepan@gmail.com>
2021-11-08 23:27:51 +0200

    Wave done

---
+++ mexicanWave.js
@@ -0,0 +1,33 @@
+function wave(str) {
+    function capitalizeLetter(str, x) {
+        let newStr = '';
+        let numOfSpace = 0;
+        for (let i = 0; i < str.length; i++) {
+            if (str.charAt(i) === ' ') {
+                numOfSpace++;
+            }
+            if (i === (x + numOfSpace)) {
+                newStr += str[i].toUpperCase();
+            } else {
+                newStr += str[i];
+            }
+
+        }
+        return newStr;
+    }
+    if (str) {
+        let len = str.match(/\w+/ig).join('').length;
+        console.log(len);
+        let arr = Array.from({
+            length: len
+        }, (v, k) => {
+            return v = capitalizeLetter(str, k);
+        })
+        return arr;
+    }
+    return [];
+}
+let r = wave("two words");
+console.log(r);
+
+
