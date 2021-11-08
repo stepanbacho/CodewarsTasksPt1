ace422eedc4be49cfa58cc29e5fbcedf03e1e764
stepanbacho <bachostepan@gmail.com>
2021-11-08 19:01:14 +0200

    diff +

--- arrayDiff.js
+++ arrayDiff.js
@@ -1,3 +1,12 @@
 function arrayDiff(a, b) {
-  
+    let arr = [];
+    a.map(function(i){
+        if(!b.includes(i)){
+            arr.push(i);
+        }
+    })
+
+    return arr;  
 }
+// let r = arrayDiff([1,8,2], []);
+// console.log(r);
