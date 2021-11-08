45d4b97d894f889ed7ebc7b05040649dd650425a
stepanbacho <bachostepan@gmail.com>
2021-11-08 13:30:26 +0200

    first commit

---
+++ phoneNum.js
@@ -0,0 +1,8 @@
+function createPhoneNumber(x){
+    let a = x.slice(0,3).join('');
+    let b = x.slice(3,6).join('');
+    let c = x.slice(6).join('');
+    return `(${a}) ${b}-${c}`
+}
+let r =createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+console.log(r);
